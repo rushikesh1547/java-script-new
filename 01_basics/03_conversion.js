9fbe60d1682e432b7f74695d63b1f11cac3fbdee
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-07-22 23:03:08 +0530

    Update 03_conversion.js

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,22 +1,68 @@
-"use strict" //treat all JS code as newer version
+let score = "33rsc"
 
-//alert(3+3)//we are using node js not browser
+//console.log(typeof score);
+//console.log(typeof (score));
 
-let name = "Rushikesh"
-let age = "18"
-let isLoggedIn = false
-let state = null
-
-//number => 2 to power 53
-//bigint
-//string => ""
-//boolean => true/false
-//null => standalone value
-//undefined =>
-//symbol => unique
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
-//object
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+
+let isLoggedIn = "Rushikesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Rushikesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
 
 
-console.log(typeof "Rushikesh")
+// *********************************** Operations *****************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Rushikesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 =2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
