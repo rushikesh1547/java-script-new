199cf303e917c9aa3f8d00c9886248fd6b8c3753
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-07-23 18:32:17 +0530

    Create dataTypes_summary.js

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,34 @@
+// 1.Primitive Datatypes
+
+// 7 types: String , Number, Boolean, NUll, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber = 3467848965863612n
+
+// 2.Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name :"Rusikesh",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
