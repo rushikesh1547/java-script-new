720081457d13b1698d7c61053b401b380d52a652
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-08-03 21:13:03 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,30 +1,30 @@
 const score = 400
-// console.log(score);
+console.log(score);
 
 const balance = new Number(100)
-// console.log(balance);
+console.log(balance);
 
-// console.log(balance.toString().length);
+console.log(balance.toString().length);
 
-// console.log(balance.toFixed(2));
+console.log(balance.toFixed(2));
 
 const otherNumber = 123.7438
 
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 
 // ************************************** Maths *****************************************
 
-// console.log(Math);
-// console.log(Math.abs(-546));
-// console.log(Math.round(3.455));
-// console.log(Math.ceil(3.4));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4 , 3 , 5 , 8));
-// console.log(Math.max(4 , 3 , 5 , 8));
+console.log(Math);
+console.log(Math.abs(-546));
+console.log(Math.round(3.455));
+console.log(Math.ceil(3.4));
+console.log(Math.floor(4.9));
+console.log(Math.min(4 , 3 , 5 , 8));
+console.log(Math.max(4 , 3 , 5 , 8));
 
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
