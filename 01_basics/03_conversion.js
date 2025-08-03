5743c9788b9c984534f30b519b60eadbf4cd9aa2
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-08-03 21:12:54 +0530

    Update 03_conversion.js

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "33rsc"
 
-//console.log(typeof score);
-//console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "Rushikesh"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0 => false
@@ -47,16 +47,16 @@ let str2 = " Rushikesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1"+ 2);
+// console.log("1"+ 2); //First no is written like it is string but the next no is normal no so it prints 12 
 
-// console.log(1 + "2");
+// console.log(1 + "2"); // same  goes with this 
 
-// console.log("1" + 2 + 2);
+// console.log("1" + 2 + 2); // in this the addition is not done of last two nos even if they are normal as compier is thinking of the y are also like 1st
 
-// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2"); // here normally addition is done of first 2 nos and the 3rd no is as it is as it is written like string
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1, num2, num3
 
