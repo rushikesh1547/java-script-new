6e2f8d8fbafc061efd0fe894c6118eab8708db96
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-08-05 22:13:33 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,4 +44,36 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("Rushikesh"))
-console.log(loginUserMessage("RUSHIKESH"))
+// console.log(loginUserMessage("RUSHIKESH"))
+
+function calculateCartPrice (val1 , val2 , ...num1){
+return num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username: "Rushikesh",
+    price: 199
+}
+
+function handelObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handelObject(user)
+handelObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200 , 400 , 100 , 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue( [200, 400, 500, 1000]));
+
