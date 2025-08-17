45416174de687700fcc742be051f205821a2494a
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-08-17 18:04:12 +0530

    Update 04_four.js

--- 05_iterations/04_four.js
+++ 05_iterations/04_four.js
@@ -19,13 +19,14 @@ for (const key in programming) {
 }
 
 
-const map = new Map()
-map.set('IN' , "India")
-map.set('USA' , "United States of America")
-map.set('FR' , "France")
-map.set('IN' , "India")
+// const map = new Map()
+// map.set('IN' , "India")
+// map.set('USA' , "United States of America")
+// map.set('FR' , "France")
+// map.set('IN' , "India")
 
-for (const key in map) {
-   console.log(key);
+// for (const key in map) {
+//    console.log(key);
    
-}
+// }
+
