82857be0689635b654e0884e24f4eb9f7979b76f
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2025-10-24 14:20:48 +0530

    Update 07_datesInJS.js

--- 01_basics/07_datesInJS.js
+++ 01_basics/07_datesInJS.js
@@ -1,8 +1,9 @@
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
 
 // let myCreatedDate = new Date(2024 , 0 ,23)
 // let myCreatedDate = new Date(2024 , 0 ,23 , 5 , 3)
